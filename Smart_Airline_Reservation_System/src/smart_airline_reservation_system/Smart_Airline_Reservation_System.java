f2c4c005dcc30afb3e4f1dddab1cff089ea32d32
Zlatin Ivalinov Tsvetkov <2249436@edu.vaniercollege.qc.ca>
2022-12-05 11:19:09 -0500

    I created the NetBeans project with all the classes

---
+++ Smart_Airline_Reservation_System/src/smart_airline_reservation_system/Smart_Airline_Reservation_System.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package smart_airline_reservation_system;
+
+/**
+ *
+ * @author 2249436
+ */
+public class Smart_Airline_Reservation_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Smart_Airline_Reservation_System/src/smart_airline_reservation_system/Ticket.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package smart_airline_reservation_system;
+
+/**
+ *
+ * @author 2249436
+ */
+public class Ticket {
+    
+}
