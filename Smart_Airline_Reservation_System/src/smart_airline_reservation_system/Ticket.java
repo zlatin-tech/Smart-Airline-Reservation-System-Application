111a179dc55b52d6dac3d7eb938ad2a89edb9d19
zlatin-tech <zlatintsvetkov@gmail.com>
2022-12-11 16:07:33 -0500

    made the constructor

--- Smart_Airline_Reservation_System/src/smart_airline_reservation_system/Ticket.java
+++ Smart_Airline_Reservation_System/src/smart_airline_reservation_system/Ticket.java
@@ -9,39 +9,79 @@ package smart_airline_reservation_system;
  * @author 2249436
  */
 public class Ticket {
+
     private Passenger passenger;
     private Flight flight;
     private double finalPrice;
     private int ticketNumber;
-    
+
     //CONSTRUCTOR
-    public Ticket(Passenger p, Flight f, double price){
-        
-    }
-    //GETTERS & SETTERS
-    public void setPassenger(Passenger p){
-        passenger = p;
-    }
-    public void setFlight(Flight f){
-        flight = f;
-    }
-    public void setFinalPrice(double price){
+    public Ticket(Passenger p, Flight f, double price) {
+        //deep copy of the passenger
+        passenger.setAge(p.getAge());
+        passenger.setEmail(p.getEmail());
+        passenger.setName(p.getName());
+        passenger.setPassengerId(p.getPassengerId());
+        passenger.setZipCode(p.getZipCode());
+        //deep copy of the flight
+        flight.setCapacity(f.getCapacity());
+        flight.setDepartureDate(f.getDepartureDate());
+        flight.setDepartureTime(f.getDepartureTime());
+        flight.setDestination(f.getDestination());
+        flight.setFlightNumber(f.getFlightNumber());
+        flight.setNumberOfSeatsLeft(f.getNumberOfSeatsLeft());
+        flight.setOrigin(f.getOrigin());
+        flight.setOriginalPrice(f.getOriginalPrice());
+        //assigning price to finalPrice
         finalPrice = price;
     }
-    public void setTicketNumber(int ticketNum){
+    public Ticket(){
+        
+    }
+
+    //GETTERS & SETTERS
+    public void setPassenger(Passenger p) {
+        passenger = p;
+    }
+
+    public void setFlight(Flight f) {
+        flight = f;
+    }
+
+    public void setFinalPrice(double price) {
+        finalPrice = price;
+    }
+
+    public void setTicketNumber(int ticketNum) {
         ticketNumber = ticketNum;
     }
-    public Passenger getPassenger(){
+
+    public Passenger getPassenger() {
         return passenger;
     }
-    public Flight getFlight(){
+
+    public Flight getFlight() {
         return flight;
     }
-    public double getFinalPrice(){
+
+    public double getFinalPrice() {
         return finalPrice;
     }
-    public int getTicketNumber(){
+
+    public int getTicketNumber() {
         return ticketNumber;
     }
-    
+
+    public String toString() {
+        String str = "";
+        str += "-------------------------------------";
+        str += str.format("%-20s: %s\n", "Flight Number", flightNumber);
+        str += str.format("%-20s: %s\n", "Origin", origin);
+        str += str.format("%-20s: %s\n", "Destination", destination);
+        str += "----------------------------";
+
+        str += "-------------------------------------";
+        return str;
+    }
+
 }
